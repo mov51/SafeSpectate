4f0dc191b9be861b42afe62cdfe112199af2b9cc
mov51 <mov51@mov51.net>
2021-04-23 07:59:06 -0400

    Initial commit

---
+++ src/main/java/net/mov51/SafeSpectate.java
@@ -0,0 +1,17 @@
+package net.mov51;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SafeSpectate extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
