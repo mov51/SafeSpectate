716825de4d63f5e36e69081ecb75332b0f10969a
mov51 <mov51@mov51.net>
2021-04-23 08:18:34 -0400

    Start on GameModeHelper

--- src/main/java/net/mov51/commands/helpers/GameModeHelper.java
+++ src/main/java/net/mov51/commands/helpers/GameModeHelper.java
@@ -1,7 +1,15 @@
 package net.mov51.commands.helpers;
 
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+
 public class GameModeHelper {
-    //todo receive user and requested game mode
-    //todo check user for game mode related permission
-    //todo set user to requested game mode
+    public void smartSetGameMode(Player p, GameMode g){
+        //receives a bukkit player object and bukkit GameMode object
+        if(p.hasPermission("SafeSpectate." + g.toString())){
+            //Generates a permission for passed GameMode using the Enum string https://hub.spigotmc.org/javadocs/bukkit/org/bukkit/GameMode.html
+            p.setGameMode(g);
+            //sets the user to the provided GameMode as long as they have the generated permission
+        }
+    }
 }
