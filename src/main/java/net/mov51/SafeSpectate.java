bca34a73188fa10c0dea074c6ba90dda984c55d9
mov51 <mov51@mov51.net>
2021-04-26 17:40:53 -0400

    Add new gmso command

--- src/main/java/net/mov51/SafeSpectate.java
+++ src/main/java/net/mov51/SafeSpectate.java
@@ -2,6 +2,7 @@ package net.mov51;
 
 import net.luckperms.api.LuckPerms;
 import net.mov51.commands.Spectate;
+import net.mov51.commands.SurvivalOverride;
 import net.mov51.commands.Survive;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.RegisteredServiceProvider;
@@ -11,11 +12,16 @@ import java.util.Objects;
 
 public final class SafeSpectate extends JavaPlugin {
 
+    public static String logPrefix = "";
+    //todo add a config for ChatPrefix value
+    public static String chatPrefix = "§6§l[§2Naspen§6§l]§r";
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-        Objects.requireNonNull(getCommand("/gmsp")).setExecutor(new Spectate());
-        Objects.requireNonNull(getCommand("/gms")).setExecutor(new Survive());
+        Objects.requireNonNull(getCommand("gmsp")).setExecutor(new Spectate());
+        Objects.requireNonNull(getCommand("gms")).setExecutor(new Survive());
+        Objects.requireNonNull(getCommand("gmso")).setExecutor(new SurvivalOverride());
 
         RegisteredServiceProvider<LuckPerms> LPprovider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
     }
--- src/main/java/net/mov51/commands/SurvivalOverride.java
+++ src/main/java/net/mov51/commands/SurvivalOverride.java
@@ -1,8 +1,6 @@
 package net.mov51.commands;
 
-import net.mov51.helpers.ChatHelper;
 import net.mov51.helpers.GameModeHelper;
-import net.mov51.helpers.lpMetaHelper;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
@@ -12,31 +10,26 @@ import org.bukkit.entity.Player;
 
 import static net.mov51.helpers.ChatHelper.sendChat;
 import static net.mov51.helpers.ChatHelper.sendWarn;
+import static net.mov51.helpers.LocationHelper.TeleportPlayer;
 import static net.mov51.helpers.LocationHelper.formatCoords;
-import static net.mov51.helpers.lpMetaHelper.saveLocation;
 import static net.mov51.helpers.lpMetaHelper.getLocation;
 
-public class Spectate implements CommandExecutor {
+public class SurvivalOverride implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(sender instanceof Player){
             Player p = (Player) sender;
-            if(p.hasPermission("SafeSpectate.Spectate")){
-                //Save location to LuckPerms
-                Location l = p.getLocation();
-                saveLocation(p, l);
-                //output location
-//                ChatHelper.sendChat("you are at" + getLocation(p),p);
-                //Set game mode to spectator
-                GameModeHelper.smartSetGameMode(p,GameMode.SPECTATOR);
-                sendChat("You will be returned to " + formatCoords(l) + " when you exit spectator mode!", p);
+            if(p.hasPermission("SafeSpectate.SpectateOverride")){
+                Location l = getLocation(p);
+                sendChat("You've overridden your return to "+ formatCoords(l) +".", p);
+                GameModeHelper.smartSetGameMode(p, GameMode.SURVIVAL);
             }else{
-                //warns player that they don't have the required permission
-                sendWarn(p,"noPerm");
+                sendWarn(p,"noOverride");
             }
         }else{
             sender.sendMessage("You must be a player to use this command!");
         }
         return false;
     }
+
 }
