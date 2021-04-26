15c82dd484b5efb3ddedfba36d906568de5e39f1
mov51 <mov51@mov51.net>
2021-04-26 11:49:20 -0400

    LuckPerms added as a dependency.

--- src/main/java/net/mov51/SafeSpectate.java
+++ src/main/java/net/mov51/SafeSpectate.java
@@ -1,7 +1,12 @@
 package net.mov51;
 
+import net.luckperms.api.LuckPerms;
 import net.mov51.commands.Spectate;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class SafeSpectate extends JavaPlugin {
 
@@ -9,7 +14,9 @@ public final class SafeSpectate extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         //todo register command
-        getCommand("SpectateSafe").setExecutor(new Spectate());
+        Objects.requireNonNull(getCommand("SpectateSafe")).setExecutor(new Spectate());
+
+        RegisteredServiceProvider<LuckPerms> LPprovider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
     }
 
     @Override
