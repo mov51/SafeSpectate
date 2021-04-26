af8e84dcc6a98519a4cc932da1a21bd153088359
mov51 <mov51@mov51.net>
2021-04-26 17:39:48 -0400

    Add preset warns for re-use

--- src/main/java/net/mov51/helpers/ChatHelper.java
+++ src/main/java/net/mov51/helpers/ChatHelper.java
@@ -1,14 +1,30 @@
 package net.mov51.helpers;
 
+import net.mov51.SafeSpectate;
 import org.bukkit.entity.Player;
+
+import static net.mov51.SafeSpectate.chatPrefix;
+import static net.mov51.SafeSpectate.logPrefix;
 
 public class ChatHelper {
 
     //prefix used for plugin messages
-    //todo add a config for value
-    public static final String PREFIX = "§6§l[§2Naspen§6§l]§r";
+
 
     public static void sendChat(String message, Player player){
-        player.sendMessage(PREFIX + message);
+        player.sendMessage(chatPrefix + message);
+    }
+
+    public static void sendLog(String message){
+        System.out.println(logPrefix + message);
+    }
+
+    public static void sendWarn(Player p, String type){
+        switch (type){
+            case "noPerm":
+                sendChat("Sorry! You don't have permission to do that! If you need help please contact staff!",p);
+            case "noOverride":
+                sendChat("Sorry! Only staff members can avoid the return teleport!",p);
+        }
     }
 }
--- src/main/java/net/mov51/helpers/GameModeHelper.java
+++ src/main/java/net/mov51/helpers/GameModeHelper.java
@@ -3,10 +3,13 @@ package net.mov51.helpers;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 
+import static net.mov51.helpers.ChatHelper.sendLog;
+
 public class GameModeHelper {
     public static void smartSetGameMode(Player p, GameMode g){
         //receives a bukkit player object and bukkit GameMode object
         p.setGameMode(g);
+        sendLog(p.getName() + " has been set to gamemode " + g);
         //sets the user to the provided GameMode as long as they have the generated permission
     }
 }
