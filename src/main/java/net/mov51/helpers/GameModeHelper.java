101a73c720904b9d65c795ff5c2fe641e781f780
mov51 <mov51@mov51.net>
2021-04-26 16:45:57 -0400

    removed auto-generated permission set. - It was kinda useless, lol

--- src/main/java/net/mov51/helpers/GameModeHelper.java
+++ src/main/java/net/mov51/helpers/GameModeHelper.java
@@ -6,10 +6,7 @@ import org.bukkit.entity.Player;
 public class GameModeHelper {
     public static void smartSetGameMode(Player p, GameMode g){
         //receives a bukkit player object and bukkit GameMode object
-        if(p.hasPermission("SafeSpectate." + g.toString())){
-            //Generates a permission for passed GameMode using the Enum string https://hub.spigotmc.org/javadocs/bukkit/org/bukkit/GameMode.html
-            p.setGameMode(g);
-            //sets the user to the provided GameMode as long as they have the generated permission
-        }
+        p.setGameMode(g);
+        //sets the user to the provided GameMode as long as they have the generated permission
     }
 }
